e2cf765464b86a188908e7669a0a66cd96c1fab6
ChrisDCP <christopherdarielc@gmail.com>
2024-10-30 13:33:28 -0600

    home page

--- appWeb.py
+++ appWeb.py
@@ -1,34 +1,41 @@
 import streamlit as st
 
+
 home = st.Page(
     page="vistas/home.py",
     title="Inicio",
+    icon="🏠",  
     default=True,
 )
 
 acerca_de_page = st.Page(
     page="vistas/acerca_de.py",
     title="Acerca de",
+    icon=":material/account_circle:",
 )
 
-project_1 = st.Page(
+project_1_page = st.Page(
     page="vistas/ventas.py",
     title="Ventas",
-
+    icon="📈",  
 )
 
-project_2 = st.Page(
+project_2_page = st.Page(
     page="vistas/chatbot.py",
-    title="Chatbot",
-
+    title="Chat Bot",
+    icon="🤖",  
 )
+
 
 pg = st.navigation(
     {
-        "informacion":{home, acerca_de_page,},
-        "proyectos":{project_1, project_2}
+        "Información:": [home, acerca_de_page],
+        "Proyectos:": [project_1_page, project_2_page],
     }
-
 )
+
+
+st.sidebar.markdown("Elaborado con ❤️ por [Streamlit](https://streamlit.io/gallery)")
+
 
 pg.run()
