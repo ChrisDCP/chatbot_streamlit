dea9c299f7aad493d5397ab76514a64f6d748c59
ChrisDCP <christopherdarielc@gmail.com>
2024-11-12 17:24:55 -0600

    vistas: acerca de y ventas

--- vistas/acerca_de.py
+++ vistas/acerca_de.py
@@ -1,3 +1,42 @@
 import streamlit as st
+from forms.contact import contact_form
+@st.dialog("contacto") 
+def ver_form_contacto():
+    contact_form()
 
-st.title("acerca de")
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1:
+    st.image('img/yo.jpg', width=230 )
+
+with  col2:
+    st.title("Christopher Castro", anchor=False)
+    st.write(
+        "Analista de datos senior, que ayuda a las empresas apoyando la toma de decisiones basada en datos. Como Ciencia de Datos."
+    )
+    if st.button("Contacto"):	# Mostrar botón ventana modal
+        ver_form_contacto()
+
+
+# --- Experiencia y calificaciones ---
+st.write("\n")
+st.subheader("Experiencia y calificaciones", anchor=False)
+st.write(
+    """
+    - 7 años de experiencia extrayendo información útil a partir de datos.
+    - Fuerte experiencia práctica y conocimiento en Python y Excel.
+    - Buen conocimiento de los principios estadísticos y sus respectivas aplicaciones.
+    - Excelente jugador de equipo y con un fuerte sentido de iniciativa en las tareas.
+    """
+)
+
+# --- HABILIDADES ---
+st.write("\n")
+st.subheader("Habilidades", anchor=False)
+st.write(
+    """
+    - Programación: Python (Scikit-learn, Pandas), SQL, VBA
+    - Visualización de Datos: PowerBi, MS Excel, Plotly
+    - Modelado: Logistic regression, linear regression, decision trees
+    - Base de Datos: Postgres, MongoDB, MySQL
+    """
+)
--- vistas/ventas.py
+++ vistas/ventas.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Ventas")
+import datetime
+
+st.subheader("filtrar datos y capturar datos")
+st.write("el procesamiento de datos a traves ciencia de datos usando Streamlit de python")
+
+dfDatos = pd.read_csv("http://raw.githubusercontent.com/gcastano/datasets/main/gapminder_data.csv")
+
+st.metric("***Registros totales***", len(dfDatos))
+st.dataframe(dfDatos, use_container_width=True)
